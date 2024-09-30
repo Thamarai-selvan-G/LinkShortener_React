cbd2b312e40af6a7335bb2c621227ef1481e1280
Thamarai-selvan-G <thamaraiselvan878@gmail.com>
2024-09-30 11:05:14 +0530

    init push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+      iam thamarai selvan
+
+     </div>
+    
+    
+     
+    </div>
+  );
+}
+
+export default App;
