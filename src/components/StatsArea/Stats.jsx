b4ff22bad230ea1cbbe2136e84d6b7ab9f022f2d
Thamarai-selvan-G <thamaraiselvan878@gmail.com>
2024-09-30 22:06:44 +0530

    cards completed

--- src/components/StatsArea/Stats.jsx
+++ src/components/StatsArea/Stats.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./Stat.css"
+import pic from "../../assets/icon-brand-recognition.svg"
+import pic2 from "../../assets/icon-fully-customizable.svg"
+import pic3 from "../../assets/icon-detailed-records.svg"
 import { useState } from 'react'
 const Stats = () => {
 
@@ -25,18 +28,41 @@ const Stats = () => {
                     <span onClick={checkInput}>Shorten It!</span>
                 {err && <p className='error'>Plese add a link</p>}
                 </div>
-
-            
                     <div className="head">
                         <h1>Advanced Statistics</h1>
                         <p>Track how your links are performing across the web with our advanced statistics dashboard</p>
                     </div>
           
                     <div className="cards">
-                        <h1>hello</h1>
-                        <h1>hello</h1>
-                        <h1>hello</h1>
-                        <h1>hello</h1>
+                       <div className="card1">
+                        <div className="cardContent">
+                            <h2>Brand Recognition</h2>
+                            <p>Boost your brand recognition with each click. Generic links don't mean a thing.Branding links help instill confidence in your content</p>
+                        </div >
+                        <div className="brand">
+                            <img src={pic} alt="" />
+                        </div>
+                       </div>
+                       <div className='line'></div>
+                       <div className="card2">
+                       <div className="cardContent">
+                            <h2>Detailed Records</h2>
+                            <p>Boost your brand recognition with each click. Generic links don't mean a thing.Branding links help instill confidence in your content</p>
+                        </div>
+                        <div className="brand">
+                            <img src={pic3} alt="" />
+                        </div>
+                       </div>
+                       <div className='line'></div>
+                       <div className="card3">
+                       <div className="cardContent">
+                            <h2>Fully Customizable</h2>
+                            <p>Boost your brand recognition with each click. Generic links don't mean a thing.Branding links help instill confidence in your content</p>
+                        </div>
+                        <div className="brand">
+                            <img src={pic2} alt="" />
+                        </div>
+                       </div>
                     </div>
             </div>
         </div>
