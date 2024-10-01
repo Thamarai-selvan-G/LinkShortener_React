3d3713463cfca541e59b153ddbfb8d3d1303a456
Thamarai-selvan-G <thamaraiselvan878@gmail.com>
2024-10-01 08:37:47 +0530

    version 1

---
+++ src/components/Footer2/Footer2.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Footer2.css"
+const Footer2 = () => {
+  return (
+    <>
+        <div className="parentFoot">
+            <div className="footContent">
+                <h2>Boost your links today</h2>
+                <span>Get started</span>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Footer2
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,6 +3,7 @@ import Nav from "../components/Navbar/Nav"
 import Landing from '../components/LandingPage/Landing'
 import Footer from '../components/Footer/Footer'
 import Stats from '../components/StatsArea/Stats'
+import Footer2 from '../components/Footer2/Footer2'
 
 const Layout = () => {
   return (
@@ -10,6 +11,7 @@ const Layout = () => {
         <Nav/>
         <Landing/>
         <Stats/>
+        <Footer2/>
         <Footer/>
     </div>
   )
